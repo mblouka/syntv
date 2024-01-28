847a6dbcf4e20b7f816bac4bb56380490d7ae9a5
LoukaMB <22691132+mblouka@users.noreply.github.com>
2024-01-28 10:49:23 -0500

    Fix for Instagram embeds.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 const redirect = {
-	"tiktok.com": "tiktxk.com",
-	"www.tiktok.com": "tiktxk.com",
-	"instagram.com": "ddinstagram.com",
-	"www.instagram.com": "ddinstagram.com",
-	"twitter.com": "vxtwitter.com",
-	"www.twitter.com": "vxtwitter.com",
-	"x.com": "fixvx.com",
-	"www.x.com": "fixvx.com",
+  "tiktok.com": "tiktxk.com",
+  "www.tiktok.com": "tiktxk.com",
+  "instagram.com": "www.ddinstagram.com",
+  "www.instagram.com": "www.ddinstagram.com",
+  "twitter.com": "vxtwitter.com",
+  "www.twitter.com": "vxtwitter.com",
+  "x.com": "fixvx.com",
+  "www.x.com": "fixvx.com",
 }
 
 export default {
-	async fetch(request) {
-		try {
-			const url = new URL(request.url)
-			const path = url.pathname.substring(1)
-			const newURL = new URL(path.startsWith("http") ? path : "https://" + path)
-			const replacement = redirect[newURL.host]
-			if (replacement) {
-				newURL.host = replacement
-			} else {
-				return new Response("Not found", { status: 404 })
-			}
-			return Response.redirect(newURL.toString(), 307)
-		} catch {
-			console.warn(`Invalid URL: ${request.url}`)
-			return new Response("Not found", { status: 404 })
-		}
-	},
+  async fetch(request) {
+    try {
+      const url = new URL(request.url)
+      const path = url.pathname.substring(1)
+      const newURL = new URL(path.startsWith("http") ? path : "https://" + path)
+      const replacement = redirect[newURL.host]
+      if (replacement) {
+        newURL.host = replacement
+      } else {
+        return new Response("Not found", { status: 404 })
+      }
+      return Response.redirect(newURL.toString(), 307)
+    } catch {
+      console.warn(`Invalid URL: ${request.url}`)
+      return new Response("Not found", { status: 404 })
+    }
+  },
 }
